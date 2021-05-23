22d3dfe94ce9c9dba2df88309b5b450e587a0bf9
WSL: Julian Toscani <julian_toscani@gmx.de>
2021-05-23 17:21:53 +0200

    set tournament ownership, restrict update and delete of tournament to owner

--- api/tournament/controllers/tournament.js
+++ api/tournament/controllers/tournament.js
@@ -5,4 +5,67 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      data.author = ctx.state.user.id;
+      entity = await strapi.services.tournament.create(data, { files });
+    } else {
+      ctx.request.body.host = ctx.state.user.id;
+      entity = await strapi.services.tournament.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.tournament });
+  },
+
+  /**
+   * Delete a record.
+   *
+   * @return {Object}
+   */
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const [tournament] = await strapi.services.tournament.find({
+      id,
+      "host.id": ctx.state.user.id,
+    });
+
+    if (!tournament) {
+      return ctx.unauthorized(`You can't delete this entry`);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.restaurant });
+  },
+
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [tournament] = await strapi.services.tournament.find({
+      id: ctx.params.id,
+      "host.id": ctx.state.user.id,
+    });
+
+    if (!tournament) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.tournament.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.tournament.update(
+        { id },
+        ctx.request.body
+      );
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.tournament });
+  },
+};
